7532f50e02c2edd1d3561ab2de05f2b2aa54873d
ManauTunjae <manau.tunjae@hotmail.com>
2026-03-30 14:15:16 +0200

    feat: create event by POST

--- src/models/Event.js
+++ src/models/Event.js
@@ -1,38 +1,45 @@
 import mongoose from "mongoose";
 
-const eventSchema = new mongoose.Schema({
-  title: {
-    type: String,
-    required: true,
-    trim: true,
+const eventSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    date: {
+      type: Date,
+      required: true,
+    },
+    location: {
+      type: String,
+      required: true,
+    },
+    maxCapacity: {
+      type: Number,
+      required: true,
+      min: 1,
+    },
+    price: {
+      type: Number,
+      required: true,
+      min: 0,
+    },
+    imageUrl: {
+      type: String,
+    },
+    category: {
+      type: String,
+      enum: ["Music", "Sport", "Theater", "Other"],
+      required: true,
+    },
   },
-  description: {
-    type: String,
-    required: true,
-  },
-  date: {
-    type: Date,
-    required: true,
-  },
-  location: {
-    type: String,
-    required: true,
-  },
-  maxCapacity: {
-    type: Number,
-    required: true,
-    min: 1,
-  },
-  imageUrl: {
-    type: String,
-  },
-  category: {
-    type: String,
-    enum: ["Music", "Sport", "Theater", "Other"],
-    required: true,
-  },
-  timestamps: true,
-});
+  { timestamps: true },
+);
 
 const Event = mongoose.model("Event", eventSchema);
 
--- src/routes/events.js
+++ src/routes/events.js
@@ -1,11 +1,55 @@
-import { Router } from 'express';
+import { Router } from "express";
+const eventRouter = Router();
+import Event from "../models/Event.js";
 
 eventRouter.get("/", async (req, res) => {
-    try {
-        
-    } catch (error) {
-        
+  try {
+    const events = await Event.find();
+    res.json(events);
+  } catch (error) {
+    console.error("Error fetching events:", error);
+  }
+});
+
+eventRouter.post("/", async (req, res) => {
+  try {
+    const {
+      title,
+      description,
+      date,
+      location,
+      maxCapacity,
+      price,
+      category,
+      imageUrl,
+    } = req.body;
+    if (
+      !title ||
+      !description ||
+      !date ||
+      !location ||
+      !maxCapacity ||
+      !price ||
+      !category
+    ) {
+      return res.status(400).json({ message: "Missing required fields" });
     }
+    const event = new Event({
+      title,
+      description,
+      date,
+      location,
+      maxCapacity,
+      price,
+      category,
+      imageUrl,
+    });
+    await event.save();
+    res.status(201).json(event);
+  } catch (error) {
+    console.error("Error creating event:", error);
+    res.status(400).json({ message: "Error creating event" });
+  }
 });
 
 export default eventRouter;
