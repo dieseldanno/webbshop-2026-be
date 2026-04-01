1bf0b8ad40b86df99b571ff546c93789795311d8
ManauTunjae <manau.tunjae@hotmail.com>
2026-04-01 10:33:26 +0200

    fix. update import event model file

--- src/db/events.js
+++ src/db/events.js
@@ -1,4 +1,4 @@
-import Event from '../models/event.js';
+import Event from './models/event.js';
 
 export async function getEvents() {
     return await Event.find().sort({ date: 1 }); // Sort by date ascending
