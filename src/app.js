fd9956c8ea24e07bd0514ae587e4e6ac39553076
ManauTunjae <manau.tunjae@hotmail.com>
2026-04-07 10:38:38 +0200

    update: delete event router and only keep events router in App.js

--- src/app.js
+++ src/app.js
@@ -3,8 +3,7 @@ import express from "express";
 import mongoose from "mongoose";
 import productsRouter from "./routes/products.js";
 import authRouter from "./routes/auth.js";
-import eventsRouter from "./routes/events.js";
-import eventRouter from "./routes/event.js";
+import eventRouter from "./routes/events.js";
 import bookingRouter from "./routes/bookings.js";
 import cors from "cors";
 
@@ -45,8 +44,7 @@ app.get("/health", (req, res) => {
 
 app.use("/products", productsRouter);
 app.use("/auth", authRouter);
-app.use("/events", eventsRouter);
-app.use("/event", eventRouter);
+app.use("/events", eventRouter);
 app.use("/bookings", bookingRouter);
 //TODO: Add more routes as needed
 
