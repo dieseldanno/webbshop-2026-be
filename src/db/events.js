de9dd90141acb001418b56e8277bd18afe4164a4
ManauTunjae <manau.tunjae@hotmail.com>
2026-04-09 12:34:14 +0200

    feat: add getEventBooking fn into db/event.js

--- src/db/events.js
+++ src/db/events.js
@@ -39,6 +39,21 @@ export async function getEventById(eventId) {
   };
 }
 
+export async function getEventBookings(eventId) {
+  const event = await Event.findById(eventId);
+  if (!event) return null;
+
+  const bookings = await Booking.find({ event: eventId }).sort({
+    createdAt: -1,
+  });
+  const totalBooked = await getTotalBookedSpots(eventId);
+  return {
+    bookings: bookings.map((b) => b.toObject()),
+    spotLeft: Math.max(0, event.maxCapacity - totalBooked),
+    totalBooked: totalBooked,
+  };
+}
+
 export async function createEvent(eventData) {
   const event = new Event(eventData);
   return await event.save();
