0969af5393881485588b11038c66be1ab235e4f8
ManauTunjae <manau.tunjae@hotmail.com>
2026-04-13 15:51:48 +0200

    feat: implement validateId and update validateEventBody &  validateEventUpdate for better validations

--- src/middleware/eventValidation.js
+++ src/middleware/eventValidation.js
@@ -10,17 +10,20 @@ export const handleValidationErrors = (req, res, next) => {
 };
 
 export const validateId = (req, res, next) => {
-    const { id } = req.params;
-    if (!mongoose.isValidObjectId(id)) {
-      return res.status(400).json({ message: "Event not found" });
-    }
-    next();
-}
+  const { id } = req.params;
+  if (!mongoose.isValidObjectId(id)) {
+    return res.status(400).json({ message: "Event not found" });
+  }
+  next();
+};
 
 export const validateEventBody = [
-  body("title").isString().withMessage("Title is required"),
-  body("description").isString().withMessage("Description is required"),
-  body("date").isISO8601().withMessage("Date must be a valid ISO 8601 date"),
+  body("title").isString().trim().withMessage("Title is required"),
+  body("description").isString().trim().withMessage("Description is required"),
+  body("date")
+    .isISO8601()
+    .custom((value) => new Date(value) > new Date())
+    .withMessage("Date must be a valid ISO 8601 date"),
   body("location")
     .isString()
     .withMessage("Location must be a string")
@@ -30,9 +33,12 @@ export const validateEventBody = [
     .isIn(["Stockholm", "Uppsala", "Göteborg", "Malmö"])
     .withMessage("Invalid location selected"),
   body("maxCapacity")
+    .custom((value) => typeof value === "number")
     .isInt({ min: 1 })
     .withMessage("Max capacity must be an integer greater than 0"),
-  body("price").isNumeric().withMessage("Price must be a number"),
+  body("price")
+    .custom((value) => typeof value === "number")
+    .withMessage("Price must be a number"),
   body("category")
     .isString()
     .withMessage("Category must be a string")
@@ -41,7 +47,7 @@ export const validateEventBody = [
     .withMessage("Category is required")
     .isIn([
       "Music",
-      "Sports",
+      "Sport",
       "Art & culture",
       "Health",
       "Dance",
@@ -53,35 +59,53 @@ export const validateEventBody = [
 ];
 
 export const validateEventUpdate = [
-    body("title").optional().isString().withMessage("Title must be a string"),
-    body("description").optional().isString().withMessage("Description must be a string"),
-    body("date").optional().isISO8601().withMessage("Date must be a valid ISO 8601 date"),
-    body("location")
-      .optional()
-      .isString()
-      .notEmpty()
-      .withMessage("Location cannot be empty")
-      .isIn(["Stockholm", "Uppsala", "Göteborg", "Malmö"])
-      .withMessage("Invalid location selected"),
-    body("maxCapacity")
-      .optional()
-      .isInt({ min: 1 })
-      .withMessage("Max capacity must be an integer greater than 0"),
-    body("price").optional().isNumeric().withMessage("Price must be a number"),
-    body("category")
-      .optional()
-      .isString()
-      .notEmpty()
-      .withMessage("Category cannot be empty")
-      .isIn([
-        "Music",
-        "Sports",
-        "Art & culture",
-        "Health",
-        "Dance",
-        "Social activities",
-      ])
-      .withMessage("Invalid category selected"),
-    body("imageUrl").optional().isURL().withMessage("Image must be a valid URL"),
-    handleValidationErrors,
-]
+  body("title")
+    .optional()
+    .isString()
+    .trim()
+    .withMessage("Title must be a string"),
+  body("description")
+    .optional()
+    .isString()
+    .trim()
+    .withMessage("Description must be a string"),
+  body("date")
+    .optional()
+    .isISO8601()
+    .custom((value) => new Date(value) > new Date())
+    .withMessage("Date must be a valid ISO 8601 date"),
+  body("location")
+    .optional()
+    .isString()
+    .trim()
+    .notEmpty()
+    .withMessage("Location cannot be empty")
+    .isIn(["Stockholm", "Uppsala", "Göteborg", "Malmö"])
+    .withMessage("Invalid location selected"),
+  body("maxCapacity")
+    .optional()
+    .custom((value) => typeof value === "number")
+    .isInt({ min: 1 })
+    .withMessage("Max capacity must be an integer greater than 0"),
+  body("price")
+    .optional()
+    .custom((value) => typeof value === "number")
+    .withMessage("Price must be a number"),
+  body("category")
+    .optional()
+    .isString()
+    .trim()
+    .notEmpty()
+    .withMessage("Category cannot be empty")
+    .isIn([
+      "Music",
+      "Sport",
+      "Art & culture",
+      "Health",
+      "Dance",
+      "Social activities",
+    ])
+    .withMessage("Invalid category selected"),
+  body("imageUrl").optional().isURL().withMessage("Image must be a valid URL"),
+  handleValidationErrors,
+];
