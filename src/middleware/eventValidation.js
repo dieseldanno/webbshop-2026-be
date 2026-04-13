36f54ae7112e29152a5098cb6c6895d661b658be
ManauTunjae <manau.tunjae@hotmail.com>
2026-04-13 14:24:32 +0200

    feat: Implement handleValidationErrors middleware using express-validator

--- src/middleware/eventValidation.js
+++ src/middleware/eventValidation.js
@@ -1,1 +1,11 @@
 //TODO: Här ska jag lägga valideringar för events
+import { body, validationResult } from 'express-validator';
+
+const handleValidationErrors = (req, res, next) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+    }
+    next();
+};
+
